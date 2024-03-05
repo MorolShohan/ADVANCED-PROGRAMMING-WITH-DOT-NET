5067b3d2e54e0299d6d3b413b242f2a2fb562c79
MorolShohan <shohan.aiubcse@gmail.com>
2024-03-05 18:30:02 +0600

    learning controller

---
+++ Learning dotnet/Learning dotnet/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Learning_dotnet.Controllers
+{
+    public class StudentController : Controller
+    {
+        public string Index()
+        {
+            return "hello from shohan";
+        }
+
+        public string Name()
+        {
+            return "My name is Shohan";
+        }
+    }
+}
