1c5105c27b96467425c5d999dd3ddc7bee51bc76
MorolShohan <shohan.aiubcse@gmail.com>
2024-03-15 17:54:09 +0600

    learning view, model and razor engine

---
+++ Learning Model/Learning Model/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Web;
+using System.Web.Mvc;
+using Learning_Model.Models;
+
+namespace Learning_Model.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var data = GetEmployee();
+            return View(data);
+        }
+
+        
+
+        private Employee GetEmployee()
+        {
+            return new Employee()
+            {
+                Id = 1,
+                Address = "Dhaka",
+                Name = "SHOHAN"
+            };
+        }
+
+    }
+}
