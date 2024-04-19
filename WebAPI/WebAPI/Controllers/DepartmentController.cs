9dcf6997b4972d3b6151aaa47d0a6dff03746b7e
MorolShohan <shohan.aiubcse@gmail.com>
2024-04-19 23:11:44 +0600

    adding webapi

---
+++ WebAPI/WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public List<Department> Get()
+        {
+            Sp24_CF_AEntities db = new Sp24_CF_AEntities();
+            return db.Departments.ToList();
+        }
+
+        public Department Get(int id)
+        {
+            return new Department { Id = id, Name = "dept" + id };
+        }
+
+
+    }
+}
