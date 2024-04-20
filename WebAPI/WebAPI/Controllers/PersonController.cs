9a43d2e956e274e7e8c42b230fc7d077ec3a7bfd
MorolShohan <shohan.aiubcse@gmail.com>
2024-04-20 11:48:39 +0600

    webapi modified

---
+++ WebAPI/WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebAPI.Models;
+using WebAPI.Models.VM;
+
+namespace WebAPI.Controllers
+{
+    public class PersonController : ApiController
+    {
+        [Route("api/person/names")]
+        [HttpGet]
+        public List<string> PrNames()
+        {
+            var data = (from pr in new Sp24_CF_AEntities().People
+                        select pr.Name).ToList();
+            return data;
+        }
+        [Route("api/person/create")]
+        [HttpPost]
+        public string Post(Person p)
+        {
+            var db = new Sp24_CF_AEntities();
+            db.People.Add(p);
+            db.SaveChanges();
+            return "added";
+
+        }
+        public List<PersonModel> Get()
+        {
+            Sp24_CF_AEntities db = new Sp24_CF_AEntities();
+            var data = new List<PersonModel>();
+            foreach (var item in db.People)
+            {
+                var person = new PersonModel()
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    DOB = item.DOB
+                };
+                data.Add(person);
+            }
+            return data;
+
+        }
+        public PersonModel Get(int id)
+        {
+            var data = new PersonModel();
+            Sp24_CF_AEntities db = new Sp24_CF_AEntities();
+            var e = db.People.FirstOrDefault(d => d.Id == id);
+            var pr = new PersonModel()
+            {
+                Id = e.Id,
+                Name = e.Name,
+                DOB = e.DOB
+            };
+            return pr;
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/VM/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models.VM
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DOB { get; set; }
+    }
+}
